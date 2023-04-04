5f8b658d7a0f0e50efc95f1df8ef4bc01ba83081
Mbaocha Jonathan <mbaocha240793@gmail.com>
2023-04-05 00:14:47 +0100

    fixed bug

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,9 +4,9 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  secret: process.env.JWT_SECRETE,
+  secret: process.env.JWT_SECRETE ?? "",
 });
